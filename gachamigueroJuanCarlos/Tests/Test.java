80b66e7625ce8022a97d5f9704077ec0d5c326a4
Juan Carlos <juagommar5@alu.edu.gva.es>
2023-05-18 20:10:36 +0200

    Test 2 y lo pasa correctamente sin modificar /src/V2

--- gachamigueroJuanCarlos/Tests/Test.java
+++ gachamigueroJuanCarlos/Tests/Test.java
@@ -5,4 +5,9 @@ public class Test {
         String result = V2.metodo("1");
         Assertions.assertEquals("1",result);
     }
+    @org.junit.jupiter.api.Test
+    public void testfor2() {
+        String result = V2.metodo("2");
+        Assertions.assertEquals("2",result);
+    }
 }
