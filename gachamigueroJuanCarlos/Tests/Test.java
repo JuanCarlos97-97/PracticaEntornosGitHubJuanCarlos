bc4dfe56a9fdafe99356c67aaf2048adc8445993
Juan Carlos <juagommar5@alu.edu.gva.es>
2023-05-18 20:16:09 +0200

    Test 3 lo pasa correctamente y se ha modificado /src/V2 para poder pasar el test3

--- gachamigueroJuanCarlos/Tests/Test.java
+++ gachamigueroJuanCarlos/Tests/Test.java
@@ -6,8 +6,13 @@ public class Test {
         Assertions.assertEquals("1",result);
     }
     @org.junit.jupiter.api.Test
-    public void testfor2() {
+    public void testFor2() {
         String result = V2.metodo("2");
         Assertions.assertEquals("2",result);
+    }
+    @org.junit.jupiter.api.Test
+    public void testFor3() {
+        String result = V2.metodo("3");
+        Assertions.assertEquals("Gacha",result);
     }
 }
--- gachamigueroJuanCarlos/src/V2.java
+++ gachamigueroJuanCarlos/src/V2.java
@@ -1,6 +1,10 @@
 public class V2 {
     public static String metodo(String n){
         String num = n;
+        int numero = Integer.parseInt(n);
+        if(numero == 3)
+            return "Gacha";
+
         return num;
     }
 }
