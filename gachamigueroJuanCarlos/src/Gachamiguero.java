b2e2a7e512a2337d621efbe8d160c778dca94a9d
Juan Carlos <juagommar5@alu.edu.gva.es>
2023-05-20 08:17:56 +0200

    En la clase Gachamiguero copiamos el método cocinar y lo volvemos a refactorizar

--- gachamigueroJuanCarlos/src/Gachamiguero.java
+++ gachamigueroJuanCarlos/src/Gachamiguero.java
@@ -1,2 +1,13 @@
 public class Gachamiguero {
+    public static String catar(String n){
+        String num = n;
+        int numero = Integer.parseInt(n);
+        if(numero%3==0 && numero%5==0)
+            return "Gachamiga";
+        if(numero%3 == 0)
+            return "Gacha";
+        if(numero % 5 == 0)
+            return "Miga";
+        return num;
+    }
 }
